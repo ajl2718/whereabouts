27dc4c75bc6d75d09afa9ba213156fca8c82fa77
alex2718 <a.lee84@hotmail.com>
2023-08-15 15:57:07 +1000

    Remove commented lines

--- setup_geocoder.py
+++ setup_geocoder.py
@@ -15,30 +15,21 @@ db_name = details['gnaf'][0]['db_name']
 gnaf_folder = details['gnaf'][2]['folder']
 states = details['geocoder'][1]['states']
 
-# folder where data is located
-# gnaf_folders = os.listdir(f'{gnaf_folder}/G-NAF')
-# data_folder = [folder_name for folder_name in gnaf_folders if 'G-NAF' in folder_name][0]
-
 # path to the auth and the state psv files
-gnaf_path = '/home/alex/Desktop/Data/GNAF/G-NAF/G-NAF MAY 2023'
 gnaf_path = '/home/alex/Desktop/Data/G-NAF Core/G-NAF Core MAY 2023/Standard/GNAF_CORE_subset.parquet'
-gnafloader.create_geocoder_tables()
-gnafloader.load_gnaf_data(gnaf_path, state_names=['SA'])
-##gnafloader.load_data(gnaf_path, states)
-#gnafloader.create_address_view()
 
-##for state in states:
-#    gnafloader.create_addresses_in_chunks(state)
-#gnafloader.create_addresses()
+print("Create geocoder tables")
+gnafloader.create_geocoder_tables()
+for state in states:
+    print(f"Importing date for {state}")
+    gnafloader.load_gnaf_data(gnaf_path, state_names=[state])
+
 gnafloader.create_final_address_table()
 
-#for state in states:
- #   gnafloader.create_addresses(state)
-
+print("Create standard phrases")
 gnafloader.create_phrases()
 gnafloader.create_inverted_index()
 
 # trigram phrases
+print("Create trigram phrases")
 gnafloader.create_phrases(['trigram'])
-
-# drop phrase table since no longer needed

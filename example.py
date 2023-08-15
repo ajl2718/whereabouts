995e267d997a3b74c8bd8ecc27e81d23141624a3
alex2718 <a.lee84@hotmail.com>
2023-08-15 16:17:46 +1000

    Simplify example geocoding script

--- example.py
+++ example.py
@@ -5,43 +5,12 @@ from whereabouts.Matcher import Matcher
 matcher = Matcher(db_name='gnaf_au')
 
 # load a dataset
-filename = '/home/alex/Desktop/Data/location_data/current_victorian_licences_by_location_18.xlsx'
-df = pd.read_excel(filename, skiprows=3)
-df = (
-    df
-    .assign(address_full = df.Address + ' ' + df.Suburb)
-    .query('address_full.isnull() == False')
-    .query('Latitude.isnull() == False')
-    .query('Longitude.isnull() == False')
-    .loc[:, ['address_full', 'Latitude', 'Longitude']]
-    .rename(columns={'Latitude': 'latitude', 'Longitude': 'longitude'})
-)
+filename = 'datasets/apartments_210223.csv'
 
-addresslist = df.address_full.values
+df = pd.read_csv(filename)
+addresslist = df.address.values
 
 t1 = time()
-results = matcher.geocode(addresslist)
+results = matcher.geocode(addresslist, how='trigram')
 t2 = time()
 print(f'Geocoded {len(results)} addresses in {t2 - t1}s')
-
-# reverse geocoding
-points = (df
-          .query('latitude.isnull() == False')
-          .query('longitude.isnull() == False')
-          .loc[:, ['latitude', 'longitude']]
-).values
-
-t1 = time()
-results = matcher.reverse_geocode(points)
-t2 = time()
-print(f'Reverse geocoded {len(results)} addresses in {t2 - t1}s')
-
-# Geocoding fixes:
-# don't use building name in comparison
-# address spans
-# incorrect suburb or postcode
-# street corners
-
-# for reverse geocoding:
-# how to match to closest polygon rather than point, e.g., when location takes up substantial space 
-# point may be within the polygon but closer to another address's centroid.

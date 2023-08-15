c4589dc560348f3ab6c30f1bf82f6a0cbb49b7b8
alex2718 <a.lee84@hotmail.com>
2023-08-15 14:15:10 +1000

    Modify geocoder setup script to create trigram tables

--- setup_geocoder.py
+++ setup_geocoder.py
@@ -21,14 +21,15 @@ states = details['geocoder'][1]['states']
 
 # path to the auth and the state psv files
 gnaf_path = '/home/alex/Desktop/Data/GNAF/G-NAF/G-NAF MAY 2023'
-
+gnaf_path = '/home/alex/Desktop/Data/G-NAF Core/G-NAF Core MAY 2023/Standard/GNAF_CORE_subset.parquet'
 gnafloader.create_geocoder_tables()
-gnafloader.load_data(gnaf_path, states)
-gnafloader.create_address_view()
+gnafloader.load_gnaf_data(gnaf_path, state_names=['SA'])
+##gnafloader.load_data(gnaf_path, states)
+#gnafloader.create_address_view()
 
 ##for state in states:
 #    gnafloader.create_addresses_in_chunks(state)
-gnafloader.create_addresses()
+#gnafloader.create_addresses()
 gnafloader.create_final_address_table()
 
 #for state in states:
@@ -38,6 +39,6 @@ gnafloader.create_phrases()
 gnafloader.create_inverted_index()
 
 # trigram phrases
-#gnafloader.create_phrases(['trigram'])
+gnafloader.create_phrases(['trigram'])
 
 # drop phrase table since no longer needed

42aaf5776581e9e4f0d802f41b6ae5ced3371e4a
alex2718 <a.lee84@hotmail.com>
2024-01-01 17:32:15 +1100

    Hash trigrams to reduce storage requirements

--- whereabouts/queries/create_trigram_index_step2.sql
+++ whereabouts/queries/create_trigram_index_step2.sql
@@ -6,6 +6,6 @@ with trigramphrase_chunk as (
     from phraseinverted_with_nums t1, numbers t2, numbers t3
     where row_num % 100 = ?
 )
-select trigramphrase, addr_ids, frequency 
+select md5(trigramphrase)[1:5], addr_ids, frequency 
 from trigramphrase_chunk
 where length(trigramphrase) >= 5

9c694636741b6ec3bd30b59735d85995bd6cc0f4
alex2718 <a.lee84@hotmail.com>
2024-01-05 16:23:06 +1100

    cast trigramphrase hashes as integers

---
+++ whereabouts/queries/create_trigram_index_step2b.sql
@@ -0,0 +1,11 @@
+insert into trigramphraseinverted
+with trigramphrase_chunk as (
+    select addr_ids, 
+    concat(str_split(t1.tokenphrase, ' ')[1][t2.col0:t2.col0+2], ' ', str_split(t1.tokenphrase, ' ')[2][t3.col0:t3.col0+2]) trigramphrase,
+    frequency
+    from phraseinverted_with_nums t1, numbers t2, numbers t3
+    where row_num % 100 = ?
+)
+select cast(hash(trigramphrase) % 1048576 as integer) trigramphrase, addr_ids, frequency -- use hashing to reduce the number of trigrams
+from trigramphrase_chunk
+where length(trigramphrase) >= 5

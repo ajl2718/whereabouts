97cdd8db62b4bd31a2b3d117f681bf79cde99db2
alex2718 <a.lee84@hotmail.com>
2024-01-29 23:00:37 +1100

    Add function for downloading files

--- whereabouts/utils.py
+++ whereabouts/utils.py
@@ -1,3 +1,6 @@
+import subprocess 
+import requests 
+
 def get_unmatched(results, threshold):
     """
     Given results (outputs from Matcher), filter out those that are correctly matched and
@@ -19,4 +22,27 @@ def order_matches(matches):
     """
     matches_sorted = sorted(matches, key=lambda k: k['address_id']) 
     return matches_sorted
+
+
+def download(model_name):
+    """
+    Download a database for geocoding addresses in a particular part of the world
+
+    Args
+    ----
+    model_name (str): name of database to download
+    """
+    # URL of location of metadata for all models
+    base_url = "https://raw.githubusercontent.com/ajl2718/whereabouts/main/model_metadata"
+    metadata_url = f"{base_url}/all_models.json"
+    # get all the model names from the reference json file
+    all_models = requests.get(metadata_url).json()['models']
+    if model_name in all_models:
+        model_metadata_url = f'{base_url}/{model_name}.json'
+        model_metadata = requests.get(model_metadata_url).json()
+        print(f"Downloading data for {model_name}")
+        data_path = model_metadata['download_url']
+        subprocess.run(["curl", "-o", f'whereabouts/models/{model_name}', data_path]) 
+    else:
+        print(f"Model {model_name} not found")
     

00ac4f27dd6df6d985625c0b99e1f41e9a1fd64a
ajl2718 <a.lee84@hotmail.com>
2024-03-10 09:50:50 +1100

    change length of trigram token hases

--- whereabouts/queries/create_trigram_index_step2b.sql
+++ whereabouts/queries/create_trigram_index_step2b.sql
@@ -6,6 +6,6 @@ with trigramphrase_chunk as (
     from phraseinverted_with_nums t1, numbers t2, numbers t3
     where row_num % 100 = ?
 )
-select cast(hash(trigramphrase) % 1048576 as integer) trigramphrase, addr_ids, frequency -- use hashing to reduce the number of trigrams
+select cast(hash(trigramphrase) % 1000000000 as integer) trigramphrase, addr_ids, frequency -- use hashing to reduce the number of trigrams
 from trigramphrase_chunk
 where length(trigramphrase) >= 5

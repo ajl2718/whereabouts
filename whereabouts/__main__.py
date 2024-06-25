77f46715a0fbf592661b2a8e18667bedb9be885a
ajl2718 <a.lee84@hotmail.com>
2024-06-25 21:39:09 +1000

    Command line functionality for downloading databases from huggingface

--- whereabouts/__main__.py
+++ whereabouts/__main__.py
@@ -1,9 +1,9 @@
 import sys
-from .utils import setup_geocoder, remove_database
+from .utils import setup_geocoder, remove_database, download
 
 def main():
     if len(sys.argv) < 3:
-        print("Usage: python -m whereabouts setup_geocoder <config_path>")
+        print("Usage: python -m whereabouts setup_geocoder <config_path> or python -m whereabouts download <db_name>")
         sys.exit(1)
 
     command = sys.argv[1]
@@ -13,6 +13,9 @@ def main():
     if command == "remove_database":
         db_name = sys.argv[2]
         remove_database(db_name)
+    if command == "download":
+        db_name = sys.argv[2]
+        download(db_name, 'saunteringcat/whereabouts-db')
     else:
         print(f'Unknown command: {command}')
 

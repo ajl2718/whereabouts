af392d1e32d6382cb7784fa58dfee3cca7e02c87
ajl2718 <a.lee84@hotmail.com>
2024-08-28 21:02:40 +1000

    Update extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
-    'sphinxcontrib.bibtex'
+    'sphinxcontrib-bibtex'
 ]
 
 intersphinx_mapping = {

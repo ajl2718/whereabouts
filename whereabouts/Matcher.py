90eebc1d74423ac26422bdd458571e2344f3c0b9
ajl2718 <a.lee84@hotmail.com>
2025-12-07 13:27:02 +1100

    Fix error handling when loading db

--- whereabouts/Matcher.py
+++ whereabouts/Matcher.py
@@ -8,13 +8,14 @@ import numpy as np
 import pandas as pd
 
 from .utils import list_overlap, numeric_overlap, ngram_jaccard
+from .errors import InvalidDatabaseError
 
 # change files to files
 DO_MATCH_BASIC = importlib.resources.files('whereabouts.queries').joinpath('geocoder_query_standard3.sql').read_text(encoding='utf-8')
 DO_MATCH_SKIPPHRASE = importlib.resources.files('whereabouts.queries').joinpath('geocoder_query_skipphrase2.sql').read_text(encoding='utf-8')
 DO_MATCH_TRIGRAM = importlib.resources.files('whereabouts.queries').joinpath('geocoder_query_trigramb3.sql').read_text(encoding='utf-8')
 CREATE_GEOCODER_TABLES = importlib.resources.files('whereabouts.queries').joinpath('create_geocoder_tables.sql').read_text(encoding='utf-8')
-    
+
 class Matcher:
     """
     A class for geocoding and reverse geocoding addresses.
@@ -60,30 +61,26 @@ class Matcher:
         if parsed_url.scheme in ('http', 'https', 'duckdb'):
             whereabouts_db = db_name
         elif parsed_url.scheme == '':
-            # Check if the model exists in the folder
+            # Check if the database is installed
             path_to_models = importlib.resources.files('whereabouts').joinpath('models')
-
             db_names = [name[:-3] for name in os.listdir(path_to_models) if name.endswith('.db')]
             if db_name in db_names:
                 whereabouts_db = f"{path_to_models}/{db_name}.db"
             else:
-                print(f"Could not find database {db_name}")
-                print("The following geocoding databases are installed:")
-                for name in db_names:
-                    print(name)
-                raise ValueError(f"Database not found: {db_name}")
+                raise InvalidDatabaseError(f"Unknown database '{db_name}'. Valid options: {'\n'.join(db_names)}") 
         else:
-            raise ValueError(f"Invalid database name or URL: {db_name}")
+            raise InvalidDatabaseError(f"Invalid database name or URL: {db_name}")
 
         # attach the whereabouts database
-        self.con.execute(f"ATTACH DATABASE '{whereabouts_db}' as remote;")
-        # Create custom functions in DuckDB connection
-        try:
-            self.con.create_function('list_overlap', list_overlap)
-            self.con.create_function('numeric_overlap', numeric_overlap)
-            self.con.create_function('ngram_jaccard', ngram_jaccard)
-        except Exception:
-            pass
+        if whereabouts_db:
+            self.con.execute(f"ATTACH DATABASE '{whereabouts_db}' as remote;")
+            # Create custom functions in DuckDB connection
+            try:
+                self.con.create_function('list_overlap', list_overlap)
+                self.con.create_function('numeric_overlap', numeric_overlap)
+                self.con.create_function('ngram_jaccard', ngram_jaccard)
+            except Exception:
+                pass
         
         self.how = how
         self.threshold = threshold

1b94195b47a817fade8033328c1997b90a44ced0
ajl2718 <a.lee84@hotmail.com>
2026-08-09 23:44:28 +1000

    style: remove unused imports, annotate intentional late imports

--- examples/create_database.py
+++ examples/create_database.py
@@ -1,15 +1,9 @@
 # Example of how to create an address matching database suitable for geocoding
 # load the test data
-import pandas as pd
 from tests.test_pipeline_components import pipeline_connection, con
-import duckdb
-import pandas as pd
-import pytest
 
-from duckdb import DuckDBPyConnection
 
 from whereabouts.QueryPipeline import QueryPipeline
-from whereabouts.matching_queries.common import register_functions
 from whereabouts.matching_queries.standard_with_neighbours import (
     clean_addresses,
     create_address_numerics,
@@ -23,12 +17,12 @@ from whereabouts.matching_queries.standard_with_neighbours import (
     compute_similarity,
     rank_by_similarity,
     select_current,
-    rejoin_all_inputs
-)        
+    rejoin_all_inputs,
+)
 
 # module fixture setup
-pc_gen = pipeline_connection.__wrapped__()   # not .wrapped()
-db = next(pc_gen)                            # value from yield
+pc_gen = pipeline_connection.__wrapped__()  # not .wrapped()
+db = next(pc_gen)  # value from yield
 
 # function fixture setup (depends on pipeline_connection)
 con_gen = con.__wrapped__(db)
@@ -40,134 +34,172 @@ pipeline.execute()
 pipeline = QueryPipeline(con=test_con, steps=[clean_addresses, create_address_numerics])
 pipeline.execute()
 
-pipeline = QueryPipeline(con=test_con, steps=[clean_addresses, create_address_numerics, create_address_alpha_tokens])
+pipeline = QueryPipeline(
+    con=test_con,
+    steps=[clean_addresses, create_address_numerics, create_address_alpha_tokens],
+)
 pipeline.execute()
 
-pipeline = QueryPipeline(con=test_con, 
-                         steps=[clean_addresses, 
-                                create_address_numerics, 
-                                create_address_alpha_tokens, 
-                                create_input_phrases])
+pipeline = QueryPipeline(
+    con=test_con,
+    steps=[
+        clean_addresses,
+        create_address_numerics,
+        create_address_alpha_tokens,
+        create_input_phrases,
+    ],
+)
 
-pipeline = QueryPipeline(con=test_con, 
-                         steps=[clean_addresses, 
-                                create_address_numerics, 
-                                create_address_alpha_tokens, 
-                                create_input_phrases, 
-                                first_matching_step])
+pipeline = QueryPipeline(
+    con=test_con,
+    steps=[
+        clean_addresses,
+        create_address_numerics,
+        create_address_alpha_tokens,
+        create_input_phrases,
+        first_matching_step,
+    ],
+)
 
-pipeline = QueryPipeline(con=test_con, 
-                         steps=[clean_addresses, 
-                                create_address_numerics, 
-                                create_address_alpha_tokens, 
-                                create_input_phrases,
-                                first_matching_step,
-                                unnest_match_candidates,
-                                ])
+pipeline = QueryPipeline(
+    con=test_con,
+    steps=[
+        clean_addresses,
+        create_address_numerics,
+        create_address_alpha_tokens,
+        create_input_phrases,
+        first_matching_step,
+        unnest_match_candidates,
+    ],
+)
 
-pipeline = QueryPipeline(con=test_con, 
-                         steps=[clean_addresses, 
-                                create_address_numerics, 
-                                create_address_alpha_tokens, 
-                                create_input_phrases,
-                                first_matching_step,
-                                unnest_match_candidates,
-                                extract_match_candidate_details,
-                                ])
+pipeline = QueryPipeline(
+    con=test_con,
+    steps=[
+        clean_addresses,
+        create_address_numerics,
+        create_address_alpha_tokens,
+        create_input_phrases,
+        first_matching_step,
+        unnest_match_candidates,
+        extract_match_candidate_details,
+    ],
+)
 
-pipeline = QueryPipeline(con=test_con, 
-                         steps=[clean_addresses, 
-                                create_address_numerics, 
-                                create_address_alpha_tokens, 
-                                create_input_phrases,
-                                first_matching_step,
-                                unnest_match_candidates,
-                                extract_match_candidate_details,
-                                filter_to_top50_candidates,
-                                ])
+pipeline = QueryPipeline(
+    con=test_con,
+    steps=[
+        clean_addresses,
+        create_address_numerics,
+        create_address_alpha_tokens,
+        create_input_phrases,
+        first_matching_step,
+        unnest_match_candidates,
+        extract_match_candidate_details,
+        filter_to_top50_candidates,
+    ],
+)
 
-pipeline = QueryPipeline(con=test_con, 
-                         steps=[clean_addresses, 
-                                create_address_numerics, 
-                                create_address_alpha_tokens, 
-                                create_input_phrases,
-                                first_matching_step,
-                                unnest_match_candidates,
-                                extract_match_candidate_details,
-                                filter_to_top50_candidates,
-                                neighbouring_suburb_match,
-                                ])
+pipeline = QueryPipeline(
+    con=test_con,
+    steps=[
+        clean_addresses,
+        create_address_numerics,
+        create_address_alpha_tokens,
+        create_input_phrases,
+        first_matching_step,
+        unnest_match_candidates,
+        extract_match_candidate_details,
+        filter_to_top50_candidates,
+        neighbouring_suburb_match,
+    ],
+)
 
-pipeline = QueryPipeline(con=test_con, 
-                         steps=[clean_addresses, 
-                                create_address_numerics, 
-                                create_address_alpha_tokens, 
-                                create_input_phrases,
-                                first_matching_step,
-                                unnest_match_candidates,
-                                extract_match_candidate_details,
-                                filter_to_top50_candidates,
-                                neighbouring_suburb_match,
-                                compute_similarity,
-                                ])
+pipeline = QueryPipeline(
+    con=test_con,
+    steps=[
+        clean_addresses,
+        create_address_numerics,
+        create_address_alpha_tokens,
+        create_input_phrases,
+        first_matching_step,
+        unnest_match_candidates,
+        extract_match_candidate_details,
+        filter_to_top50_candidates,
+        neighbouring_suburb_match,
+        compute_similarity,
+    ],
+)
 
-pipeline = QueryPipeline(con=test_con, 
-                         steps=[clean_addresses, 
-                                create_address_numerics, 
-                                create_address_alpha_tokens, 
-                                create_input_phrases,
-                                first_matching_step,
-                                unnest_match_candidates,
-                                extract_match_candidate_details,
-                                filter_to_top50_candidates,
-                                neighbouring_suburb_match,
-                                compute_similarity,
-                                ])
+pipeline = QueryPipeline(
+    con=test_con,
+    steps=[
+        clean_addresses,
+        create_address_numerics,
+        create_address_alpha_tokens,
+        create_input_phrases,
+        first_matching_step,
+        unnest_match_candidates,
+        extract_match_candidate_details,
+        filter_to_top50_candidates,
+        neighbouring_suburb_match,
+        compute_similarity,
+    ],
+)
 
-pipeline = QueryPipeline(con=test_con, 
-                         steps=[clean_addresses, 
-                                create_address_numerics, 
-                                create_address_alpha_tokens, 
-                                create_input_phrases,
-                                first_matching_step,
-                                unnest_match_candidates,
-                                extract_match_candidate_details,
-                                filter_to_top50_candidates,
-                                neighbouring_suburb_match,
-                                compute_similarity,
-                                rank_by_similarity,
-                                ])
+pipeline = QueryPipeline(
+    con=test_con,
+    steps=[
+        clean_addresses,
+        create_address_numerics,
+        create_address_alpha_tokens,
+        create_input_phrases,
+        first_matching_step,
+        unnest_match_candidates,
+        extract_match_candidate_details,
+        filter_to_top50_candidates,
+        neighbouring_suburb_match,
+        compute_similarity,
+        rank_by_similarity,
+    ],
+)
 
-pipeline = QueryPipeline(con=test_con, 
-                         steps=[clean_addresses, 
-                                create_address_numerics, 
-                                create_address_alpha_tokens, 
-                                create_input_phrases,
-                                first_matching_step,
-                                unnest_match_candidates,
-                                extract_match_candidate_details,
-                                filter_to_top50_candidates,
-                                neighbouring_suburb_match,
-                                compute_similarity,
-                                rank_by_similarity,
-                                select_current,
-                                ])
+pipeline = QueryPipeline(
+    con=test_con,
+    steps=[
+        clean_addresses,
+        create_address_numerics,
+        create_address_alpha_tokens,
+        create_input_phrases,
+        first_matching_step,
+        unnest_match_candidates,
+        extract_match_candidate_details,
+        filter_to_top50_candidates,
+        neighbouring_suburb_match,
+        compute_similarity,
+        rank_by_similarity,
+        select_current,
+    ],
+)
 
-pipeline = QueryPipeline(con=test_con, 
-                         steps=[clean_addresses, 
-                                create_address_numerics, 
-                                create_address_alpha_tokens, 
-                                create_input_phrases,
-                                first_matching_step,
-                                unnest_match_candidates,
-                                extract_match_candidate_details,
-                                filter_to_top50_candidates,
-                                neighbouring_suburb_match,
-                                compute_similarity,
-                                rank_by_similarity,
-                                select_current,
-                                rejoin_all_inputs
-                                ])
+pipeline = QueryPipeline(
+    con=test_con,
+    steps=[
+        clean_addresses,
+        create_address_numerics,
+        create_address_alpha_tokens,
+        create_input_phrases,
+        first_matching_step,
+        unnest_match_candidates,
+        extract_match_candidate_details,
+        filter_to_top50_candidates,
+        neighbouring_suburb_match,
+        compute_similarity,
+        rank_by_similarity,
+        select_current,
+        rejoin_all_inputs,
+    ],
+)
 
 # teardown (same order pytest would do)
 next(con_gen, None)
